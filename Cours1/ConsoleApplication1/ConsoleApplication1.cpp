8444c56c3177f6bee4bcbc2aebe3a132ded2790b
Akananas <francois.charrier23@gmail.com>
2020-09-24 14:23:41 +0200

    Vector2 et Vector3

--- Cours1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Cours1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,18 +1,22 @@
-// ConsoleApplication1.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
 
 #include "pch.h"
 #include <iostream>
+#include "Vector2.hpp"
+#include "Vector3.hpp"
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n"; 
+int main() {
+	Vector3 v1(3, 2, 5);
+	Vector3 v2(4, 6, 1);
+	Vector3 cross = v1.Cross(v2);
+	cout << to_string(cross.x) << " " << to_string(cross.y) << " " << to_string(cross.z) << " " << to_string(cross.Mag());
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
-// Conseils pour bien démarrer : 
+// Astuces pour bien démarrer : 
 //   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
 //   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
 //   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
