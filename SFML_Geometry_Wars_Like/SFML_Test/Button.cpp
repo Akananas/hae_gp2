d7a3eaa2c7b7ccd101760f07c33bdc021d123292
Akananas <41255457+Akananas@users.noreply.github.com>
2021-01-10 17:16:15 +0100

    Fix bug hover

--- SFML_Geometry_Wars_Like/SFML_Test/Button.cpp
+++ SFML_Geometry_Wars_Like/SFML_Test/Button.cpp
@@ -1,6 +1,6 @@
 #include "Button.hpp"
 
-void Button::mouseHover(Vector2f) {
+void Button::mouseHover(bool hovering) {
 	if (hovering) {
 		buttonShape.setTexture(&hoverTex);
 	}
@@ -8,6 +8,20 @@ void Button::mouseHover(Vector2f) {
 		buttonShape.setTexture(&idleTex);
 	}
 	isHovering = hovering;
+}
+
+Button::Button(std::string path, sf::Vector2f pos, std::function<void()> function) {
+	idleTex.loadFromFile(path + "idle.png");
+	hoverTex.loadFromFile(path + "hover.png");
+	clickedTex.loadFromFile(path + "clicked.png");
+	buttonShape.setSize(sf::Vector2f(48, 48));
+	buttonShape.setOrigin(24, 24);
+	buttonShape.setPosition(pos);
+	onClick = function;
+}
+
+void Button::SetTexture() {
+	buttonShape.setTexture(&idleTex);
 }
 
 void Button::UpdateButton(sf::Vector2f mousePos) {
--- SFML_Geometry_Wars_Like/SFML_Test/Button.hpp
+++ SFML_Geometry_Wars_Like/SFML_Test/Button.hpp
@@ -10,35 +10,10 @@ public:
 	bool isHovering = false;
 	std::function<void()> onClick;
 	Button(){}
-	Button(std::string path, sf::Vector2f pos, std::function<void()> function) {
-		idleTex.loadFromFile(path + "idle.png");
-		hoverTex.loadFromFile(path + "hover.png");
-		clickedTex.loadFromFile(path + "clicked.png");
-		buttonShape.setSize(sf::Vector2f(48, 48));
-		buttonShape.setOrigin(24, 24);
-		buttonShape.setPosition(pos);
-		onClick = function;
-	}
-	void SetTexture() {
-		buttonShape.setTexture(&idleTex);
-	}
-	void UpdateButton(sf::Vector2f mousePos) {
-		if (buttonShape.getGlobalBounds().contains(mousePos)) {
-			mouseHover(true);
-		}
-		else {
-			mouseHover(false);
-		}
-	}
-	void mouseHover(bool hovering) {
-		if (hovering) {
-			buttonShape.setTexture(&hoverTex);
-		}
-		else {
-			buttonShape.setTexture(&idleTex);
-		}
-		isHovering = hovering;
-	}
+	Button(std::string path, sf::Vector2f pos, std::function<void()> function);
+	void SetTexture();
+	void UpdateButton(sf::Vector2f mousePos);
+	void mouseHover(bool hovering);
 
 	void mouseClick() {
 		buttonShape.setTexture(&clickedTex);
